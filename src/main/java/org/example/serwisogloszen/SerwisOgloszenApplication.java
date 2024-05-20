cacdf51435b7dc0362d8afec0e38ccc37b53e58f
PawelKrol23 <p.krol18a@gmail.com>
2024-05-20 18:45:21 +0200

    initial commit

---
+++ src/main/java/org/example/serwisogloszen/SerwisOgloszenApplication.java
@@ -0,0 +1,13 @@
+package org.example.serwisogloszen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisOgloszenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisOgloszenApplication.class, args);
+    }
+
+}
